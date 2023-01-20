91110df3dd70d21adeec6382c4745b8d13bfe489
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-01-19 16:39:53 -0800

    able to collect player name from ESPN NBA injury list

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.static('client/dist'));
 app.use(express.json());
 
+const playerInjuries = 'https://www.espn.com/nba/injuries';
+
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
@@ -20,6 +24,23 @@ app.get('/*', function(req, res) {
   })
 })
 
+const information = [];
+
+axios(playerInjuries)
+  .then (response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const playerName = $('.col-name.Table__TD');
+
+    playerName.each((idx, ref) => {
+      const name = $(ref);
+      console.log(name.text())
+    })
+
+  })
+
+
+
 app.listen(Number(process.env.PORT),()=>{
-  console.log('listening on port 3000');
+  console.log(`listening on port ${process.env.PORT}`);
 })
