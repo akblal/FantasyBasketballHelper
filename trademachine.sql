a5c21cbb55d97b60efa30702803dce011569b599
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-01-21 14:26:37 -0800

    created psql table for all players and stats

--- trademachine.sql
+++ trademachine.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS injury;
+DROP TABLE IF EXISTS allPlayers;
 
 CREATE TABLE injury (
   name TEXT NOT NULL,
@@ -8,4 +9,36 @@ CREATE TABLE injury (
   comment TEXT
 );
 
+CREATE TABLE allPlayers (
+  name TEXT NOT NULL,
+  team TEXT NOT NULL,
+  position TEXT NOT NULL,
+  age TEXT NOT NULL,
+  gamesPlayed TEXT,
+  mpg TEXT,
+  minPercent TEXT,
+  usgPercent TEXT,
+  toPercent TEXT,
+  fta TEXT,
+  ftPercent TEXT,
+  twopa TEXT,
+  twopPercent TEXT,
+  threepa TEXT,
+  threepPercent TEXT,
+  efgPercent TEXT,
+  tsPercent TEXT,
+  ppg TEXT,
+  rpg TEXT,
+  trbPercent TEXT,
+  apg TEXT,
+  astPercent TEXT,
+  spg TEXT,
+  bpg TEXT,
+  topg TEXT,
+  vi TEXT,
+  ortg TEXT,
+  drtg TEXT
+);
+
 \COPY injury FROM 'injuryList.csv' DELIMITERS ',' CSV;
+\COPY allPlayers FROM 'playerStats.csv' DELIMITERS ',' CSV;
