43c9eb4954a3594ed17efca57628e4f885658648
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-01-27 00:48:39 -0800

    able to display players by positions after clicking on the team card

--- client/src/components/DisplayOriginalTeam/DisplayOriginalTeam.jsx
+++ client/src/components/DisplayOriginalTeam/DisplayOriginalTeam.jsx
@@ -6,7 +6,12 @@ import { TeamContext } from '../../App.jsx';
 function DisplayOriginalTeam () {
 
   const {team} = useContext(TeamContext);
-  const [selectedTeam, setSelectedTeam] = useState(null)
+  const [selectedTeam, setSelectedTeam] = useState([]);
+  const [guard, setGuard] = useState([]);
+  const [forward, setForward] = useState([]);
+  const [smallForward, setSmallForward] = useState([]);
+  const [center, setCenter] = useState([]);
+  const [powerForward, setPowerForward] = useState([]);
 
   useEffect(() => {
     getPlayers();
@@ -21,7 +26,30 @@ function DisplayOriginalTeam () {
     .then((results) => {
       let players = results.data.rows
       setSelectedTeam(players.slice())
-      console.log(players)
+      let tempGuard = [];
+      let tempForward = [];
+      let tempCenter = [];
+      let tempSmallForward = [];
+      let tempPowerForward = [];
+      for (let i = 0; i < players.length; i++) {
+        let player = players[i];
+        if (player.position === 'G') {
+          tempGuard.push(player)
+        } else if (player.position === 'F') {
+          tempForward.push(player)
+        } else if (player.position === 'G-F' || player.position === 'F-G') {
+          tempSmallForward.push(player)
+        } else if (player.position === 'F-C' || player.position === 'C-F') {
+          tempPowerForward.push(player)
+        } else {
+          tempCenter.push(player)
+        }
+      }
+      setGuard(tempGuard.slice());
+      setForward(tempForward.slice());
+      setSmallForward(tempSmallForward.slice());
+      setPowerForward(tempPowerForward.slice());
+      setCenter(tempCenter.slice());
     })
     .catch ((err) => {
       console.log (err, 'err from get request in DisplayOriginalTeam')
@@ -31,8 +59,35 @@ function DisplayOriginalTeam () {
   return (
   <div>
     <h1>list of players on {team.name}</h1>
-    {selectedTeam ? selectedTeam.map((player) => {
-      return <h2 key= {player.name}>{player.name}</h2>
+    {guard.length ?
+      <h2>Guard</h2>
+      : null}
+    {guard.length ? guard.map((player) => {
+      return <h4 key= {player.name}>{player.name}</h4>
+    }) : null}
+    {smallForward.length ?
+      <h2>Small Forward</h2>
+      : null}
+    {smallForward.length ? smallForward.map((player) => {
+      return <h4 key= {player.name}>{player.name}</h4>
+    }) : null}
+    {forward.length ?
+      <h2>Forward</h2>
+      : null}
+    {forward.length ? forward.map((player) => {
+      return <h4 key= {player.name}>{player.name}</h4>
+    }) : null}
+    {powerForward.length ?
+      <h2>Power Forward</h2>
+      : null}
+    {powerForward.length ? powerForward.map((player) => {
+      return <h4 key= {player.name}>{player.name}</h4>
+    }) : null}
+    {center.length ?
+      <h2>Center</h2>
+      : null}
+    {center.length ? center.map((player) => {
+      return <h4 key= {player.name}>{player.name}</h4>
     }) : null}
   </div>
   )
